b69c4fe60c0dee8c551e739e6715c663c649fddc
kyler <kyler>
2017-03-23 08:26:35 -0700

    create build directory if not exist

--- build.bat
+++ build.bat
@@ -6,7 +6,11 @@ set projectDirectory=%~dp0
 set buildToolsPath="C:\Program Files (x86)\Microsoft Visual C++ Build Tools"
 
 call %buildToolsPath%\vcbuildtools.bat %platform%
+
 cd /d %projectDirectory%
+if not exist build\windows (
+   mkdir build\windows
+)
 pushd build\windows
 cl -Zi ..\..\src\win32_calendar.cpp user32.lib gdi32.lib
 popd
