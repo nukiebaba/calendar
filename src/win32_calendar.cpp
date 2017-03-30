9a544ba77ecc4524d0837b1b9126079234d492f3
Kyler Nelson <bluemachine10@comcast.net>
2017-03-30 11:45:00 -0700

    win32 Initital window and message handling

--- src/win32_calendar.cpp
+++ src/win32_calendar.cpp
@@ -13,9 +13,6 @@ struct platform_window
     int Height;
 };
 
-
-
-
 void
 PlatformDrawClock(platform_window* Window)
 {
@@ -66,6 +63,58 @@ PlatformCloseWindow(platform_window* Window)
     }
 }
 
+LRESULT CALLBACK
+MainWindowCallback(HWND Window,
+                   UINT Message,
+                   WPARAM WParam,
+                   LPARAM LParam)
+{
+    LRESULT Result = 0;
+    
+    switch(Message)
+    {
+        case WM_SIZE:
+        {
+            OutputDebugStringA("WM_SIZE\n");
+        } break;
+
+        case WM_DESTROY:
+        {
+            OutputDebugStringA("WM_DESTROY\n");
+        } break;
+
+        case WM_CLOSE:
+        {
+            PostQuitMessage(0);
+            OutputDebugStringA("WM_CLOSE\n");
+        } break;
+
+        case WM_ACTIVATEAPP:
+        {
+            OutputDebugStringA("WM_ACTIVEAPP\n");
+        } break;
+
+        case WM_PAINT:
+        {
+            PAINTSTRUCT Paint;
+            HDC DeviceContext = BeginPaint(Window, &Paint);
+            int X = Paint.rcPaint.left;
+            int Y = Paint.rcPaint.top;
+            LONG Width = Paint.rcPaint.right - Paint.rcPaint.left;
+            LONG Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
+            PatBlt(DeviceContext, X, Y, Width, Height, WHITENESS);
+            EndPaint(Window, &Paint);
+        } break;
+
+        default:
+        {
+            Result = DefWindowProc(Window, Message, WParam, LParam);
+        } break;
+    }
+
+    return Result;
+}
+
 int CALLBACK
 WinMain(HINSTANCE Instance,
         HINSTANCE PrevInstance,
@@ -74,9 +123,51 @@ WinMain(HINSTANCE Instance,
 {
     WNDCLASS WindowClass = {};
 
-    
+    WindowClass.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
+    WindowClass.lpfnWndProc = MainWindowCallback;
+    WindowClass.hInstance = Instance;
+    //    WindowClass.hIcon;
+    WindowClass.lpszClassName = "CalendarWindowClass";
 
-    //GameMain(0, NULL, NULL);
+    if( RegisterClass(&WindowClass) )
+    {
+        HWND WindowHandle =
+            CreateWindowEx(0,
+                           WindowClass.lpszClassName,
+                           "Calendar",
+                           WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+                           CW_USEDEFAULT, //x
+                           CW_USEDEFAULT, //y
+                           CW_USEDEFAULT, //Width
+                           CW_USEDEFAULT, //Height
+                           0,
+                           0,
+                           Instance,
+                           0);
+        if( WindowHandle )
+        {
+            MSG Message;
+            for(;;)
+            {
+                BOOL MessageResult = GetMessage(&Message, 0, 0, 0);
+                if( MessageResult > 0 )
+                {
+                    TranslateMessage(&Message); //Prepares message
+                    DispatchMessage(&Message);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+    else
+    {
+        // @Logging
+    }
+    
+    GameMain(0, NULL, NULL);
     
     return 0;
 }
