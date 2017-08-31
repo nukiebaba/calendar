95a73e52e51a3e038fc4192b472db33be0f7437b
relic <bluemachine10@comcast.net>
2017-08-30 22:57:13 -0700

    create subdirectories if not exist

--- build.sh
+++ build.sh
@@ -6,12 +6,20 @@ echo Working directory: $directory
 if [ -d $directory ]; then
     if uname -a | grep 'CYGWIN_NT'; then
         buildDirectory="$directory/build/cygwin"
+        if [ ! -d "$buildDirectory" ]; then
+            mkdir -p "$buildDirectory"
+        fi
+
         $directory/resources/ctime.exe -begin $directory/calendar.ctm
         clang $directory/src/linux_calendar.cpp -o $buildDirectory/calendar -g
         error_code=$?
         $directory/resources/ctime.exe -end $directory/calendar.ctm "${error_code}"
     else
         buildDirectory="$directory/build/linux"
+        if [ ! -d "$buildDirectory" ]; then
+            mkdir -p "$buildDirectory"
+        fi
+
         $directory/resources/ctime -begin $directory/calendar.ctm
         clang $directory/src/linux_calendar.cpp -o $buildDirectory/calendar -lX11 -lm -g
         error_code=$?
